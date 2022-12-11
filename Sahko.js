1e31ccc5ed0d7eca186b3c5998889ad1a7204faa
xxxronixxx <58064308+xxxronixxx@users.noreply.github.com>
2022-12-12 00:09:51 +0200

    Update Sahko.js

--- Sahko.js
+++ Sahko.js
@@ -1,12 +1,17 @@
-var hours = Array.from({ length: 24 }, () => Math.floor(Math.random() * 100));
-
+if (sessionStorage.hours) {
+    var hours = JSON.parse(sessionStorage.hours);
+    //hours.toArray();
+} else {
+    var data = Array.from({ length: 24 }, () => Math.floor(Math.random() * 100));
+    sessionStorage.hours = JSON.stringify(data);
+    var hours = data;
+}
 var cheapest = Math.min(...hours)
-
 
 const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
 });
-// Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+// Get the value of "?" in eg "https://localhost/?halvimmat_tunnit=?"
 let value = params.halvimmat_tunnit; // "halvimmat_tunnit"
 var price = false;
 if (cheapest == hours[value]) price = true;
@@ -15,6 +20,6 @@ const result = {
     halvin: price,
 }
 
-console.log(cheapest);
 console.log(hours);
+console.log("halvin on " + cheapest);
 console.log(result);
